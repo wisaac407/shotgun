ee719e6165e140f9e4ab4fe2b4ce6c8caf866740
Isaac Weaver <wisaac407@gmail.com>
2017-04-23 16:58:37 -0500

    Remove many redundant hotkeys

--- shotgun.py
+++ shotgun.py
@@ -56,17 +56,8 @@ with KeyMap(kc, 'UV Editor', space_type='EMPTY', region_type='WINDOW', modal=Fal
 
     kmi = km.keymap_items.new('uv.cursor_set', 'ACTIONMOUSE', 'PRESS', ctrl=True)
 
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
-
-    kmi = km.keymap_items.new('wm.context_menu_enum', 'TAB', 'PRESS', shift=True, ctrl=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.snap_uv_element')
-
 # Map Mask Editing
 with KeyMap(kc, 'Mask Editing', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
-
     kmi = km.keymap_items.new('mask.add_vertex_slide', 'SELECTMOUSE', 'PRESS', ctrl=True)
 
     kmi = km.keymap_items.new('mask.select_all', 'A', 'PRESS', ctrl=True)
@@ -122,12 +113,8 @@ with KeyMap(kc, 'Clip Dopesheet Editor', space_type='CLIP_EDITOR', region_type='
     kmi = km.keymap_items.new('clip.dopesheet_select_channel', 'SELECTMOUSE', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'extend', True)
 
-    kmi = km.keymap_items.new('clip.dopesheet_view_all', 'HOME', 'PRESS')
-
 # Map Outliner
 with KeyMap(kc, 'Outliner', space_type='OUTLINER', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('outliner.scroll_page', 'PAGE_DOWN', 'PRESS')
-
     kmi = km.keymap_items.new('outliner.selected_toggle', 'A', 'PRESS', ctrl=True)
 
 # Map Graph Editor
@@ -163,13 +150,8 @@ with KeyMap(kc, 'Graph Editor', space_type='GRAPH_EDITOR', region_type='WINDOW',
 
     kmi = km.keymap_items.new('graph.click_insert', 'SELECTMOUSE', 'CLICK', ctrl=True)
 
-    kmi = km.keymap_items.new('marker.rename', 'M', 'PRESS', ctrl=True)
-
 # Map 3D View
 with KeyMap(kc, '3D View', space_type='VIEW_3D', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('view3d.manipulator', 'LEFTMOUSE', 'PRESS', any=True)
-    kmi_props_setattr(kmi.properties, 'release_confirm', True)
-
     kmi = km.keymap_items.new('view3d.cursor3d', 'ACTIONMOUSE', 'PRESS', ctrl=True)
 
     kmi = km.keymap_items.new('view3d.move', 'RIGHTMOUSE', 'PRESS')
@@ -221,17 +203,8 @@ with KeyMap(kc, 'Pose', space_type='EMPTY', region_type='WINDOW', modal=False) a
 
 # Map Object Mode
 with KeyMap(kc, 'Object Mode', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
-
     kmi = km.keymap_items.new('object.select_all', 'A', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'action', 'TOGGLE')
-
-    kmi = km.keymap_items.new('object.location_clear', 'G', 'PRESS', alt=True)
-
-    kmi = km.keymap_items.new('object.rotation_clear', 'R', 'PRESS', alt=True)
-
-    kmi = km.keymap_items.new('object.scale_clear', 'S', 'PRESS', alt=True)
 
     kmi = km.keymap_items.new('wm.call_menu', 'A', 'PRESS')
     kmi_props_setattr(kmi.properties, 'name', 'VIEW3D_MT_object_apply')
@@ -247,41 +220,23 @@ with KeyMap(kc, 'Curve', space_type='EMPTY', region_type='WINDOW', modal=False) 
 
     kmi = km.keymap_items.new('curve.delete', 'DEL', 'PRESS')
 
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
-
 # Map Metaball
 with KeyMap(kc, 'Metaball', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
     kmi = km.keymap_items.new('mball.select_all', 'A', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'action', 'TOGGLE')
-
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
 
 # Map Lattice
 with KeyMap(kc, 'Lattice', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
     kmi = km.keymap_items.new('lattice.select_all', 'A', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'action', 'TOGGLE')
 
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
-
 # Map Particle
 with KeyMap(kc, 'Particle', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
     kmi = km.keymap_items.new('particle.select_all', 'A', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'action', 'TOGGLE')
 
-    kmi = km.keymap_items.new('view3d.manipulator', 'LEFTMOUSE', 'PRESS', any=True)
-    kmi_props_setattr(kmi.properties, 'release_confirm', True)
-
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
-
 # Map Node Editor
 with KeyMap(kc, 'Node Editor', space_type='NODE_EDITOR', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('wm.call_menu', 'W', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'name', 'AMTH_NODE_MT_amaranth_templates')
-
     kmi = km.keymap_items.new('wm.call_menu', 'EQUAL', 'PRESS', shift=True)
     kmi_props_setattr(kmi.properties, 'name', 'NODE_MT_nw_node_align_menu')
 
@@ -304,8 +259,6 @@ with KeyMap(kc, 'Timeline', space_type='TIMELINE', region_type='WINDOW', modal=F
 
 # Map Mesh
 with KeyMap(kc, 'Mesh', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('mesh.shortest_path_pick', 'SELECTMOUSE', 'PRESS', ctrl=True)
-
     kmi = km.keymap_items.new('mesh.select_all', 'A', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'action', 'TOGGLE')
 
@@ -318,9 +271,6 @@ with KeyMap(kc, 'Mesh', space_type='EMPTY', region_type='WINDOW', modal=False) a
 
     kmi = km.keymap_items.new('mesh.dupli_extrude_cursor', 'SELECTMOUSE', 'CLICK', shift=True, ctrl=True)
     kmi_props_setattr(kmi.properties, 'rotate_source', False)
-
-    kmi = km.keymap_items.new('wm.context_cycle_enum', 'O', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.proportional_edit_falloff')
 
 # Map Armature
 with KeyMap(kc, 'Armature', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
@@ -344,53 +294,6 @@ with KeyMap(kc, 'View2D', space_type='EMPTY', region_type='WINDOW', modal=False)
 
 # Map Image Paint
 with KeyMap(kc, 'Image Paint', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
-    kmi = km.keymap_items.new('wm.radial_control', 'F', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path_primary', 'tool_settings.image_paint.brush.size')
-    kmi_props_setattr(kmi.properties, 'data_path_secondary', 'tool_settings.unified_paint_settings.size')
-    kmi_props_setattr(kmi.properties, 'use_secondary', 'tool_settings.unified_paint_settings.use_unified_size')
-    kmi_props_setattr(kmi.properties, 'rotation_path', 'tool_settings.image_paint.brush.mask_texture_slot.angle')
-    kmi_props_setattr(kmi.properties, 'color_path', 'tool_settings.image_paint.brush.cursor_color_add')
-    kmi_props_setattr(kmi.properties, 'fill_color_path', 'tool_settings.image_paint.brush.color')
-    kmi_props_setattr(kmi.properties, 'zoom_path', 'space_data.zoom')
-    kmi_props_setattr(kmi.properties, 'image_id', 'tool_settings.image_paint.brush')
-    kmi_props_setattr(kmi.properties, 'secondary_tex', True)
-
-    kmi = km.keymap_items.new('wm.radial_control', 'F', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'data_path_primary', 'tool_settings.image_paint.brush.strength')
-    kmi_props_setattr(kmi.properties, 'data_path_secondary', 'tool_settings.unified_paint_settings.strength')
-    kmi_props_setattr(kmi.properties, 'use_secondary', 'tool_settings.unified_paint_settings.use_unified_strength')
-    kmi_props_setattr(kmi.properties, 'rotation_path', 'tool_settings.image_paint.brush.mask_texture_slot.angle')
-    kmi_props_setattr(kmi.properties, 'color_path', 'tool_settings.image_paint.brush.cursor_color_add')
-    kmi_props_setattr(kmi.properties, 'fill_color_path', 'tool_settings.image_paint.brush.color')
-    kmi_props_setattr(kmi.properties, 'zoom_path', '')
-    kmi_props_setattr(kmi.properties, 'image_id', 'tool_settings.image_paint.brush')
-    kmi_props_setattr(kmi.properties, 'secondary_tex', True)
-
-    kmi = km.keymap_items.new('wm.radial_control', 'F', 'PRESS', ctrl=True)
-    kmi_props_setattr(kmi.properties, 'data_path_primary', 'tool_settings.image_paint.brush.texture_slot.angle')
-    kmi_props_setattr(kmi.properties, 'data_path_secondary', '')
-    kmi_props_setattr(kmi.properties, 'use_secondary', '')
-    kmi_props_setattr(kmi.properties, 'rotation_path', 'tool_settings.image_paint.brush.texture_slot.angle')
-    kmi_props_setattr(kmi.properties, 'color_path', 'tool_settings.image_paint.brush.cursor_color_add')
-    kmi_props_setattr(kmi.properties, 'fill_color_path', 'tool_settings.image_paint.brush.color')
-    kmi_props_setattr(kmi.properties, 'zoom_path', '')
-    kmi_props_setattr(kmi.properties, 'image_id', 'tool_settings.image_paint.brush')
-    kmi_props_setattr(kmi.properties, 'secondary_tex', False)
-
-    kmi = km.keymap_items.new('wm.radial_control', 'F', 'PRESS', ctrl=True, alt=True)
-    kmi_props_setattr(kmi.properties, 'data_path_primary', 'tool_settings.image_paint.brush.mask_texture_slot.angle')
-    kmi_props_setattr(kmi.properties, 'data_path_secondary', '')
-    kmi_props_setattr(kmi.properties, 'use_secondary', '')
-    kmi_props_setattr(kmi.properties, 'rotation_path', 'tool_settings.image_paint.brush.mask_texture_slot.angle')
-    kmi_props_setattr(kmi.properties, 'color_path', 'tool_settings.image_paint.brush.cursor_color_add')
-    kmi_props_setattr(kmi.properties, 'fill_color_path', 'tool_settings.image_paint.brush.color')
-    kmi_props_setattr(kmi.properties, 'zoom_path', '')
-    kmi_props_setattr(kmi.properties, 'image_id', 'tool_settings.image_paint.brush')
-    kmi_props_setattr(kmi.properties, 'secondary_tex', True)
-
-    kmi = km.keymap_items.new('brush.stencil_control', 'RIGHTMOUSE', 'PRESS', alt=True)
-    kmi_props_setattr(kmi.properties, 'mode', 'TRANSLATION')
-
     kmi = km.keymap_items.new('wm.context_menu_enum', 'R', 'PRESS')
     kmi_props_setattr(kmi.properties, 'data_path', 'tool_settings.image_paint.brush.texture_angle_source_random')
 
@@ -399,51 +302,11 @@ with KeyMap(kc, 'Sequencer', space_type='SEQUENCE_EDITOR', region_type='WINDOW',
     kmi = km.keymap_items.new('sequencer.select_all', 'A', 'PRESS', ctrl=True)
     kmi_props_setattr(kmi.properties, 'action', 'TOGGLE')
 
-    kmi = km.keymap_items.new('sequencer.select', 'SELECTMOUSE', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'extend', False)
-    kmi_props_setattr(kmi.properties, 'linked_handle', False)
-    kmi_props_setattr(kmi.properties, 'left_right', False)
-    kmi_props_setattr(kmi.properties, 'linked_time', False)
-
-    kmi = km.keymap_items.new('sequencer.select', 'SELECTMOUSE', 'PRESS', shift=True)
-    kmi_props_setattr(kmi.properties, 'extend', True)
-    kmi_props_setattr(kmi.properties, 'linked_handle', False)
-    kmi_props_setattr(kmi.properties, 'left_right', False)
-    kmi_props_setattr(kmi.properties, 'linked_time', False)
-
-    kmi = km.keymap_items.new('sequencer.select', 'SELECTMOUSE', 'PRESS', alt=True)
-    kmi_props_setattr(kmi.properties, 'extend', False)
-    kmi_props_setattr(kmi.properties, 'linked_handle', True)
-    kmi_props_setattr(kmi.properties, 'left_right', False)
-    kmi_props_setattr(kmi.properties, 'linked_time', False)
-
-    kmi = km.keymap_items.new('sequencer.select', 'SELECTMOUSE', 'PRESS', shift=True, alt=True)
-    kmi_props_setattr(kmi.properties, 'extend', True)
-    kmi_props_setattr(kmi.properties, 'linked_handle', True)
-    kmi_props_setattr(kmi.properties, 'left_right', False)
-    kmi_props_setattr(kmi.properties, 'linked_time', False)
-
-    kmi = km.keymap_items.new('sequencer.select', 'SELECTMOUSE', 'PRESS', ctrl=True)
-    kmi_props_setattr(kmi.properties, 'extend', False)
-    kmi_props_setattr(kmi.properties, 'linked_handle', False)
-    kmi_props_setattr(kmi.properties, 'left_right', True)
-    kmi_props_setattr(kmi.properties, 'linked_time', True)
-
-    kmi = km.keymap_items.new('sequencer.select', 'SELECTMOUSE', 'PRESS', shift=True, ctrl=True)
-    kmi_props_setattr(kmi.properties, 'extend', True)
-    kmi_props_setattr(kmi.properties, 'linked_handle', False)
-    kmi_props_setattr(kmi.properties, 'left_right', False)
-    kmi_props_setattr(kmi.properties, 'linked_time', True)
-
-    kmi = km.keymap_items.new('marker.rename', 'M', 'PRESS', ctrl=True)
-
     kmi = km.keymap_items.new('view2d.pan', 'RIGHTMOUSE', 'PRESS')
 
 # Map Animation
 with KeyMap(kc, 'Animation', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
     kmi = km.keymap_items.new('anim.change_frame', 'SELECTMOUSE', 'DOUBLE_CLICK')
-
-    kmi = km.keymap_items.new('anim.previewrange_clear', 'P', 'PRESS', alt=True)
 
 # Map Image
 with KeyMap(kc, 'Image', space_type='IMAGE_EDITOR', region_type='WINDOW', modal=False) as km:
@@ -454,46 +317,6 @@ with KeyMap(kc, 'Image', space_type='IMAGE_EDITOR', region_type='WINDOW', modal=
 
     kmi = km.keymap_items.new('image.sample', 'SELECTMOUSE', 'PRESS')
 
-    kmi = km.keymap_items.new('object.mode_set', 'TAB', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'mode', 'OBJECT')
-    kmi_props_setattr(kmi.properties, 'toggle', True)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'ONE', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 0)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'TWO', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 1)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'THREE', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 2)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'FOUR', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 3)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'FIVE', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 4)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'SIX', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 5)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'SEVEN', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 6)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'EIGHT', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 7)
-
-    kmi = km.keymap_items.new('wm.context_set_int', 'NINE', 'PRESS')
-    kmi_props_setattr(kmi.properties, 'data_path', 'space_data.image.render_slot')
-    kmi_props_setattr(kmi.properties, 'value', 8)
-
 # Map Markers
 with KeyMap(kc, 'Markers', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
     kmi = km.keymap_items.new('marker.move', 'EVT_TWEAK_S', 'ANY', alt=True)
@@ -503,8 +326,6 @@ with KeyMap(kc, 'Markers', space_type='EMPTY', region_type='WINDOW', modal=False
 # Map Animation Channels
 with KeyMap(kc, 'Animation Channels', space_type='EMPTY', region_type='WINDOW', modal=False) as km:
     kmi = km.keymap_items.new('anim.channels_select_all_toggle', 'A', 'PRESS', ctrl=True)
-
-    kmi = km.keymap_items.new('anim.channels_ungroup', 'G', 'PRESS', alt=True)
 
 # Map Info
 with KeyMap(kc, 'Info', space_type='INFO', region_type='WINDOW', modal=False) as km:
