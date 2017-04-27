d411c5f090525f4d73fdd6f01346e69da2a91919
Isaac Weaver <wisaac407@gmail.com>
2017-04-27 12:49:56 -0500

    Allow opening a file with blender.sh script
    Makes it more convenient testing specific files

--- blender.sh
+++ blender.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start blender in default mode and load shotgun. Useful for testing.
-blender --factory-startup --python-expr \
+blender $1 --factory-startup --python-expr \
     "import bpy; bpy.ops.wm.keyconfig_import(filepath='shotgun.py'); bpy.context.user_preferences.inputs.select_mouse = 'LEFT'"
