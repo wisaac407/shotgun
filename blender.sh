6fa5d42a7e4c60ebd848a27fd674d82dacdf6b58
Isaac Weaver <wisaac407@gmail.com>
2017-04-27 17:42:52 -0500

    Don't overwrite existing keymap with `blender.sh`
    Previously running `blender.sh` would result in Blender overwriting the
    existing shotgun keymap with the development version. Now it's a little
    more sandboxed.

--- blender.sh
+++ blender.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Prevent blender from overwrite any existing shotgun keymap
+SHOTGUN=$(mktemp)
+cp shotgun.py ${SHOTGUN}
+
 # Start blender in default mode and load shotgun. Useful for testing.
 blender $1 --factory-startup --python-expr \
-    "import bpy; bpy.ops.wm.keyconfig_import(filepath='shotgun.py'); bpy.context.user_preferences.inputs.select_mouse = 'LEFT'"
+    "import bpy; bpy.ops.wm.keyconfig_import(filepath='${SHOTGUN}'); bpy.context.user_preferences.inputs.select_mouse = 'LEFT'"
+
+rm ${SHOTGUN}
