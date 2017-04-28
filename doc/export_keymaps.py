efc8e9852a210a0a1003649252d523cc07dccfbf
Isaac Weaver <wisaac407@gmail.com>
2017-04-28 14:57:35 -0500

    Load blender docs inventory file directly from blender

--- doc/export_keymaps.py
+++ doc/export_keymaps.py
@@ -72,7 +72,7 @@ version = '{version}'
 release = '{version}'
 
 intersphinx_mapping = {{
-    'blender': ('http://docs.blender.org/api/blender_python_api_master/', '{blender_objects}')
+    'blender': ('http://docs.blender.org/api/blender_python_api_master/', None)
 }}
 """
 
