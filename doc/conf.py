992acbd1139b8ae5e38fefb3059ae225316eb364
Travis CI <wisaac407@gmail.com>
2017-04-29 03:22:59 +0000

    Travis build: 54

--- doc/conf.py
+++ doc/conf.py
@@ -17,8 +17,8 @@ project = 'Shotgun'
 copyright = '2017, Isaac Weaver'
 author = 'Isaac Weaver'
 
-version = ' - c3086e2'
-release = ' - c3086e2'
+version = 'v0.2.2 - c3086e2'
+release = 'v0.2.2 - c3086e2'
 
 intersphinx_mapping = {
     'blender': ('http://docs.blender.org/api/blender_python_api_master/', '../blender_objects.inv')
