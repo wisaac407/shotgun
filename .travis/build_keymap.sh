d797bbbb49bf26b1e96b8df62632d8b973ef5f6c
Isaac Weaver <wisaac407@gmail.com>
2017-05-04 16:54:56 -0500

    Export to temp.py then move to shotgun.py in travis build

--- .travis/build_keymap.sh
+++ .travis/build_keymap.sh
@@ -3,11 +3,12 @@
 # Start blender, import the keymap then export it.
 # The exported keymap will have the entire keymap so blender can load it on startup.
 blender --background --factory-startup --python-expr \
- "import bpy; bpy.ops.wm.keyconfig_import(filepath='shotgun.py'); bpy.ops.wm.keyconfig_export(filepath='shotgun.py')"
+ "import bpy; bpy.ops.wm.keyconfig_import(filepath='shotgun.py'); bpy.ops.wm.keyconfig_export(filepath='temp.py')"
 
 BANNER="# Shotgun keymap. More info at http://shotgun.readthedocs.io
 VERSION = '$(git describe --abbrev=0)'
 
 "
 
-printf "${BANNER}" | cat - shotgun.py > temp; mv temp shotgun.py
+printf "${BANNER}" | cat - temp.py > shotgun.py
+rm temp.py
