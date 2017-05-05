4264823c392e2a3e7386924c3ba09cec5d44787f
Isaac Weaver <wisaac407@gmail.com>
2017-05-05 16:58:50 -0500

    Prevent making tons of tmp keymaps when testing

--- blender.sh
+++ blender.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Prevent blender from overwrite any existing shotgun keymap
-SHOTGUN=$(mktemp)
-cp shotgun.py ${SHOTGUN}
-
 # Start blender in default mode and load shotgun. Useful for testing.
 blender $1 --factory-startup --python-expr \
-    "import bpy; bpy.ops.wm.keyconfig_import(filepath='${SHOTGUN}'); bpy.context.user_preferences.inputs.select_mouse = 'LEFT'"
-
-rm ${SHOTGUN}
+    "import bpy; bpy.ops.wm.keyconfig_activate(filepath='shotgun.py'); bpy.context.user_preferences.inputs.select_mouse = 'LEFT'"
